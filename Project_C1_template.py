02795c569074922e2c2f65191e884717f3678add
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-03 20:15:32 -0800

    Add files via upload

--- Project_C1_template.py
+++ Project_C1_template.py
@@ -8,8 +8,8 @@ screen = pygame.display.set_mode((400,600))
 
 face=pygame.Rect(100,200,200,200)
 eye1=pygame.Rect(120,240,60,30)
-eye2=pygame.Rect(220,240,60,30)
-
+eye2=pygame.Rect(220,240,60,30) 
+mouth=pygame.Rect(165,320,70,30)    
 while True:    
     screen.fill((0,0,0))
     for event in pygame.event.get():
@@ -17,11 +17,10 @@ while True:
             pygame.quit()
             sys.exit()
          
-    pygame.draw.rect(screen,(120,120,120),face) 
-    pygame.draw.rect(screen,(225,225,00),eye1)
-    pygame.draw.rect(screen,(225,225,00),eye2)
-    
-    
+    pygame.draw.rect(screen,(120,120,120),face)  
+    pygame.draw.rect(screen,(225,225,0),eye1) 
+    pygame.draw.rect(screen,(225,225,0),eye2) 
+    pygame.draw.rect(screen,(225,150,150),mouth)
     pygame.display.update()
     clock.tick(30)
 
